a2ec85b70e4c7cda580649486e85054a7fdd8f99
Gal919 <galraidi00@gmail.com>
2022-05-28 17:54:19 +0300

    add header component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      <Header titelName= 'My Todos'/>
+    </div>
+  );
+}
+
+export default App;
